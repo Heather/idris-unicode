bc442e1793f8feb9b2443a41fcd3d1dcdb3d237c
Heather <heather@live.ru>
2015-03-01 22:19:58 +0400

    add small test

--- test/runtests.sh
+++ test/runtests.sh
@@ -12,7 +12,9 @@ clean_up() {
 clean_up
 
 echo "compiling tests..."
-idris Test.idr -p Control.Unicode -o test || die "* could not compile tests *"
+idris Test.idr -p Control.Unicode -o test || die "* could not compile test *"
+idris rust-example.idr -p Control.Unicode -o rust || die "* could not compile test *"
 
 echo "compiled OK, running tests..."
 ./test || die "* test failed or timed out *"
+./rust || die "* test failed or timed out *"
