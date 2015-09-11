3c46c811d2c7b659a83a8a0cfe369ad3746eb5e2
Heather <heather@live.ru>
2015-09-11 13:59:31 +0400

    comparing Int and Nat with converting Nat to Int ...

---
+++ test/Basics.idr
@@ -0,0 +1,14 @@
+module Main
+
+import Unicode
+
+natToInt : Nat → ℤ
+natToInt O = 0
+natToInt (S k) = 1 + (natToInt k)
+
+main : ໒ ()
+main = do
+  let i : ℤ = 10
+  let n : ℕ = 5
+  let b = i > (natToInt n)
+  putStrLn $ ✪ b
--- test/runtests.sh
+++ test/runtests.sh
@@ -12,9 +12,11 @@ clean_up() {
 clean_up
 
 echo "compiling tests..."
-idris Test.idr -p Control.Unicode -p contrib -o test || die "* could not compile test *"
-idris rust-example.idr -p Control.Unicode -o rust || die "* could not compile test *"
+idris Basics.idr -p Control.Unicode -p contrib -o base || die "* could not compile basics test *"
+idris Test.idr -p Control.Unicode -p contrib -o test || die "* could not compile default test *"
+idris rust-example.idr -p Control.Unicode -o rust || die "* could not compile rust example *"
 
 echo "compiled OK, running tests..."
-./test || die "* test failed or timed out *"
-./rust || die "* test failed or timed out *"
+./base || die "* base test failed or timed out *"
+./test || die "* default test failed or timed out *"
+./rust || die "* rust example failed or timed out *"
